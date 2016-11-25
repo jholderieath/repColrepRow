7150cd7c1b2fcec1de3c3cfa350ed52436b0ce19
zejasonh18 <jason.holderieath@colostate.edu>
2016-11-24 17:44:19 -0700

    try this again

--- R/repColrepRow.R
+++ R/repColrepRow.R
@@ -1,14 +1,24 @@
-#'Two functions to duplicate columns
+#'Two functions to duplicate rows and columns
 #'
 #'These functions allow easy replication of rows and columns
-#'@param x column to be repeated
+#'@param x row/column to be repeated
 #'@param n number of times to be repeated
 #'@return matrix with x repeated n times
 #'@seealso https://www.r-bloggers.com/a-quick-way-to-do-row-repeat-and-col-repeat-rep-row-rep-col/ and is written by TszKin Julian
-#'@examples repCol(x,n)
-#'@export
-#'repCol()
+#'@examples \dontrun{repRow(x,n)}
+#'@name repColrepRow
+NULL
 
+#' @rdname repColrepRow
+#' @export
+repRow <- function(x,n){
+  matrix(rep(x,each=n),nrow=n)
+}
+
+#' @rdname repColrepRow
+#' @export
 repCol <- function(x,n){
   matrix(rep(x,each=n), ncol=n, byrow=TRUE)
 }
+
+
